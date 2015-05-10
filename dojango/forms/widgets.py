a77b10669e20606a0ec645fdc8359b7ab9f9dc22
Yasir Assam <mail@endlessvoid.com>
2015-05-10 14:58:38 +1000

    Upgrade to Django 1.8

--- dojango/forms/widgets.py
+++ dojango/forms/widgets.py
@@ -20,7 +20,7 @@ __all__ = (
     'DojoWidgetMixin', 'Input', 'Widget', 'TextInput', 'PasswordInput',
     'HiddenInput', 'MultipleHiddenInput', 'FileInput', 'Textarea',
     'DateInput', 'DateTimeInput', 'TimeInput', 'CheckboxInput', 'Select',
-    'NullBooleanSelect', 'SelectMultiple', 'RadioInput', 'RadioFieldRenderer',
+    'NullBooleanSelect', 'SelectMultiple', 'RadioFieldRenderer',
     'RadioSelect', 'CheckboxSelectMultiple', 'MultiWidget', 'SplitDateTimeWidget',
     'SplitHiddenDateTimeWidget', 'SimpleTextarea', 'EditorInput', 'HorizontalSliderInput',
     'VerticalSliderInput', 'ValidationTextInput', 'ValidationPasswordInput',
@@ -285,7 +285,7 @@ class NullBooleanSelect(DojoWidgetMixin, widgets.NullBooleanSelect):
 class SelectMultiple(DojoWidgetMixin, widgets.SelectMultiple):
     dojo_type = 'dijit.form.MultiSelect'
 
-RadioInput = widgets.RadioInput
+#RadioInput = widgets.RadioInput
 RadioFieldRenderer = widgets.RadioFieldRenderer
 
 class RadioSelect(DojoWidgetMixin, widgets.RadioSelect):
